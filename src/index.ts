9c69dc06e34b236cad63d4dd3cd2fd2bd20615a0
adri <asampere@grupocdv.com>
2025-04-03 18:18:54 +0200

    Configurando docker y conexión a DB

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import server from './server'
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
 import express from 'express'
+import db from './config/db'
 import productRouter from './routes/productRouter'
+import colors from 'colors'
 
 const server = express()
+
+// DB connection
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.bgGreen.white('Database connected successfully'))
+    } catch (error) {
+        console.log(colors.bgRed.white('Unable to connect to the database'))
+        console.error(error)
+    }
+}
+
+connectDB()
 
 // Configure routes
 server.get('/status', (req, res) => {
